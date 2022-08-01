8f2d96214233f7d32ec6a05065d615e93234de0c
Paulo Sérgio <ps131296@gmail.com>
2022-08-01 20:46:36 -0300

    make AddAccountModel public

--- Domain/AddAccount.swift
+++ Domain/AddAccount.swift
@@ -1,10 +1,17 @@
 import Foundation
 
-struct AddAccountModel {
-    let name: String
-    let email: String
-    let password: String
-    let passwordConfirmation: String
+public struct AddAccountModel {
+    public let name: String
+    public let email: String
+    public let password: String
+    public let passwordConfirmation: String
+
+    public init (name: String, email: String, password: String, passwordConfirmation: String) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
 }
 
 struct AccountModel {
