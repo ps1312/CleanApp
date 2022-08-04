d21c4dd26e2dab6c28dcef4c2492fb5302df9e31
Paulo Sérgio <ps131296@gmail.com>
2022-08-04 09:32:13 -0300

    ensure AlamofireAdapter makes request with correct URL (using a URLProtocol stub to verify)

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,52 @@
+import XCTest
+import Alamofire
+
+class AlamofireAdapter {
+    let session: Session
+
+    init(session: Session = .default) {
+        self.session = session
+    }
+
+    func post(to url: URL) {
+        session.request(url).resume()
+    }
+}
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func testPostMakesRequestWithCorrectURL() {
+        let exp = expectation(description: "waiting for request observation")
+        let expectedURL = URL(string: "https://www.specific-url.com")!
+
+        let sessionConfiguration = URLSessionConfiguration.default
+        sessionConfiguration.protocolClasses = [URLProtocolStub.self]
+
+        let sut = AlamofireAdapter(session: Session(configuration: sessionConfiguration))
+
+        sut.post(to: expectedURL)
+
+        URLProtocolStub.observeRequest = { capturedURL in
+            XCTAssertEqual(capturedURL, expectedURL)
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 0.1)
+    }
+
+}
+
+class URLProtocolStub: URLProtocol {
+
+    static var observeRequest: ((URL?) -> Void)? = nil
+
+    override class func canInit(with request: URLRequest) -> Bool { return true }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest { return request }
+
+    override func startLoading() {
+        URLProtocolStub.observeRequest?(request.url)
+    }
+
+    override func stopLoading() { }
+}
