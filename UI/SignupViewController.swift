de12ca1abb69e688c5f4a4d9e7ccfe9a290dbcba
Paulo Sérgio <ps131296@gmail.com>
2022-08-11 21:12:58 -0300

    ensure SignupViewController implements LoadingView

---
+++ UI/SignupViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Presentation
+
+class SignupViewController: UIViewController {
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension SignupViewController: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        if viewModel.isLoading {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+    }
+}
--- UITests/SignupViewControllerTests.swift
+++ UITests/SignupViewControllerTests.swift
@@ -1,7 +1,22 @@
 import XCTest
+@testable import UI
 
 class SignupViewControllerTests: XCTestCase {
-    func test() {
+    func testSignupViewControllerDoesNotDisplayLoadingOnStart() {
+        let bundle = Bundle(for: SignupViewController.self)
+        let storyboard = UIStoryboard(name: "Main", bundle: bundle)
+        let sut = storyboard.instantiateInitialViewController() as! SignupViewController
 
+        sut.loadViewIfNeeded()
+
+        XCTAssertFalse(sut.isDisplayingLoading())
+
+    }
+}
+
+
+private extension SignupViewController {
+    func isDisplayingLoading() -> Bool {
+        return activityIndicator.isAnimating
     }
 }
